b6596d7e3a870eb8d52003af3f5e547f1d7d81fb
artem <nostro32@mail.ru>
2022-06-09 14:48:31 +0300

    change error messages

--- utils/input_process.py
+++ utils/input_process.py
@@ -5,108 +5,108 @@ from utils.dictionary import pair_dict
 
 
 class FastaRead():
-	def __init__(self, file):
-		self.file_name = file
+    def __init__(self, file):
+        self.file_name = file
 
-	def __enter__(self):
-		self.file = open(self.file_name, 'r')
-		self.gen = self.iter_func()
-		return self
+    def __enter__(self):
+        self.file = open(self.file_name, 'r')
+        self.gen = self.iter_func()
+        return self
 
-	def __exit__(self, _type, value, traceback):
-		self.file.close()
+    def __exit__(self, _type, value, traceback):
+        self.file.close()
 
-	def __iter__(self):
-		return self.gen
+    def __iter__(self):
+        return self.gen
 
-	def __next__(self):
-		return next(self.gen)
+    def __next__(self):
+        return next(self.gen)
 
-	def __len__(self):
-		self.reopen()
-		self.gen = self.iter_func()
-		
-		n=0
-		for _ in self.gen:
-			n+=1
-			
-		self.reopen()
-		self.gen = self.iter_func()
-		return n
-		
-	def reopen(self):
-		self.file.close()
-		self.file = open(self.file_name, 'r')
+    def __len__(self):
+        self.reopen()
+        self.gen = self.iter_func()
+        
+        n=0
+        for _ in self.gen:
+            n+=1
+            
+        self.file.seek(0)
+        self.gen = self.iter_func()
+        return n
+        
+    def reopen(self):
+        self.file.close()
+        self.file = open(self.file_name, 'r')
 
-	def iter_func(self):
-		name=None
-		
-		for l in self.file:
-			if l.isspace():
-				continue
-			if l[0] == '>':
-				if name is not None:
-					raise ValueError(f'Name "{name}" without sequence')
-				name = l[1:-1]
-			
-			else:
-				if name is None:
-					raise ValueError(f'Sequence without name "{l[:-1]}"')
-				
-				yield name, l[:-1]
-				name=None
+    def iter_func(self):
+        name=None
+        
+        for l in self.file:
+            if l.isspace():
+                continue
+            if l[0] == '>':
+                if name is not None:
+                    raise ValueError(f'Name "{name}" without sequence. (Note that all sequence names must start with ">" symbol)')
+                name = l[1:-1]
+            
+            else:
+                if name is None:
+                    raise ValueError(f'Sequence without name "{l[:-1]}". (Note that all sequence names must start with ">" symbol)')
+                
+                yield name, l[:-1]
+                name=None
 
 
 def validate_seq(seq):
-	if len(seq)>256:
-		warnings.warn(f'Max sequence length is 256, got {len(seq)}')
-		return None
-	s = []
-	for c in seq:
-		uc = c.upper()
-		if uc in 'AUGCIN':
-			s.append(uc)
-		elif uc == 'T':
-			s.append('U')
-		else:
-			warnings.warn(f'unknown symbol {c}, changed to N')
-			s.append('N')
- 			
-	return ''.join(s)
+    if len(seq)>256:
+        warnings.warn(f'Max sequence length is 256, got {len(seq)}')
+        return None
+    s = []
+    for c in seq:
+        uc = c.upper()
+        if uc in 'AUGCIN':
+            s.append(uc)
+        elif uc == 'T':
+            s.append('U')
+        else:
+            warnings.warn(f'unknown symbol {c}, changed to N')
+            s.append('N')
+             
+    return ''.join(s)
 
 
 def pad(seq):
-	left = (256 - len(seq))// 2
-	right = 256 - len(seq) - left
-	return 'N'*left + seq + 'N'*right
+    left = (256 - len(seq))// 2
+    right = 256 - len(seq) - left
+    return 'N'*left + seq + 'N'*right
 
 
 def make_mtx(seq):
-	leng = len(seq)
-	M = np.zeros((leng, leng))
-	for n in range(leng):
-		for p in range(n-1):
-			fx = seq[n]
-			fy = seq[p]
+    leng = len(seq)
+    M = np.zeros((leng, leng))
+    for n in range(leng):
+        for p in range(n-1):
+            fx = seq[n]
+            fy = seq[p]
 
-			fx1 = ''
-			fy1 = ''
-			
-			if n<leng-1:
-				fx1 = seq[n+1]
-			if p<leng-1:
-				fy1 = seq[p+1]
-          	
-			M[n][p] = pair_dict[fx+fx1+'/'+fy1+fy]
-			M[p][n] = pair_dict[fy+fy1+'/'+fx1+fx]
+            fx1 = ''
+            fy1 = ''
             
-	return M
+            if n<leng-1:
+                fx1 = seq[n+1]
+            if p<leng-1:
+                fy1 = seq[p+1]
+              
+            M[n][p] = pair_dict[fx+fx1+'/'+fy1+fy]
+            M[p][n] = pair_dict[fy+fy1+'/'+fx1+fx]
+            
+    return M
     
     
 def seq2matrix(seq):
     seq = validate_seq(seq)
     if seq is None:
-    	return None
+        return None
     seq = pad(seq)
     matrix = make_mtx(seq)
     
--- utils/output_process.py
+++ utils/output_process.py
@@ -3,17 +3,17 @@ import numpy as np
 
 
 def probable_triang_matrix(M, sensitivity = 0.5):
-    mol_size = M.shape[-1]
-    diag = (np.diag(np.ones((mol_size)))==0)
-    fm = np.zeros((mol_size, mol_size))
+    seq_size = M.shape[-1]
+    diag = (np.diag(np.ones((seq_size)))==0)
+    fm = np.zeros((seq_size, seq_size))
     
     thmask = M>sensitivity
     s = M*diag*thmask
 
     while np.sum(s)>0:
       m = np.argmax(s)
-      r = m//mol_size
-      c = m%mol_size
+      r = m//seq_size
+      c = m%seq_size
 
       fm[r,c] = 1
       s[r] = 0
@@ -26,76 +26,76 @@ def probable_triang_matrix(M, sensitivity = 0.5):
 
 
 def get_struct_layers(vec):
-	layers = []
-	op = []
-	end = []
-	
-	for i in range(vec.shape[0]):
-		if vec[i]==0:
-			continue
-		if len(op)==0:
-			op.append(i)
-			end.append(vec[i])
-			continue
+    layers = []
+    op = []
+    end = []
+    
+    for i in range(vec.shape[0]):
+        if vec[i]==0:
+            continue
+        if len(op)==0:
+            op.append(i)
+            end.append(vec[i])
+            continue
 
-		if i-op[-1]==1 and end[-1]-vec[i]==1:
-			op.append(i)
-			end.append(vec[i])
-		else:
-			layers.append((tuple(op), tuple(end)))
-			op = [i]
-			end = [vec[i]]
+        if i-op[-1]==1 and end[-1]-vec[i]==1:
+            op.append(i)
+            end.append(vec[i])
+        else:
+            layers.append((tuple(op), tuple(end)))
+            op = [i]
+            end = [vec[i]]
 
-	if len(op)!=0:
-		layers.append((tuple(op), tuple(end)))
+    if len(op)!=0:
+        layers.append((tuple(op), tuple(end)))
 
-	return layers
+    return layers
     
     
 def assemble_from_layers(layers, mol_size):
-	types = {0:'()', 1:'[]', 2:'{}', 3:'<>', 4:'Aa', 5:'Bb', 6:'Cc', 7:'Dd', 8:'Ee'}
-	intersection = [0 for _ in range(len(layers))]
-	dots = ['.' for _ in range(mol_size)]
+    types = {0:'()', 1:'[]', 2:'{}', 3:'<>', 4:'Aa', 5:'Bb', 6:'Cc', 7:'Dd', 8:'Ee'}
+    intersection = [0 for _ in range(len(layers))]
+    dots = ['.' for _ in range(mol_size)]
     
-	def intersect(l, prel):
-		if l[0][-1]<prel[1][0] and l[1][0]>prel[1][-1]:# .(((..[[..)))..]].
-			return True
+    def intersect(l, prel):
+        if l[0][-1]<prel[1][0] and l[1][0]>prel[1][-1]:# .(((..[[..)))..]].
+            return True
     
-	for l in range(len(layers)):
-		layer = layers[l]
+    for l in range(len(layers)):
+        layer = layers[l]
         
-		int_types = []
-		for prel in range(l):
-			if intersect(layer, layers[prel]):
-				int_types.append(intersection[prel])
-		
-		types_set = set(int_types)
-		if len(types_set)>=9:
-			return None
-		typ = min(set([i for i in range(9)]) - types_set)# lowest type of brackets
-		intersection[l] = typ
-		        
-		brackets = types[typ]
-		for c in range(len(layer[0])):
-			dots[layer[0][c]] = brackets[0]# open bracket
-			dots[layer[1][c]] = brackets[1]# close bracket
-	
-	return ''.join(dots)
+        int_types = []
+        for prel in range(l):
+            if intersect(layer, layers[prel]):
+                int_types.append(intersection[prel])
+        
+        types_set = set(int_types)
+        if len(types_set)>=9:
+            return None
+        typ = min(set([i for i in range(9)]) - types_set)# lowest type of brackets
+        intersection[l] = typ
+                
+        brackets = types[typ]
+        for c in range(len(layer[0])):
+            dots[layer[0][c]] = brackets[0]# open bracket
+            dots[layer[1][c]] = brackets[1]# close bracket
+    
+    return ''.join(dots)
 
 
 def matrix2struct(M, sensitivity = 0.5):
-	M = probable_triang_matrix(M, sensitivity = sensitivity)
-	vec = np.argmax(M, axis=-1)
-	layers = get_struct_layers(vec)
-	struct = assemble_from_layers(layers, vec.shape[0])
-	
-	return struct
-	
-	
+    M = probable_triang_matrix(M, sensitivity = sensitivity)
+    vec = np.argmax(M, axis=-1)
+    layers = get_struct_layers(vec)
+    struct = assemble_from_layers(layers, vec.shape[0])
+    
+    return struct
+    
+    
 def unpad(struct, seq):
-	left = (256 - len(seq))// 2
-	right = 256 - len(seq) - left
-	return struct[left:(left+len(seq))]
+    left = (256 - len(seq))// 2
+    right = 256 - len(seq) - left
+    return struct[left:(left+len(seq))]
 
 
 
